e2229ff18c3ae3ba86b05e7a996a45e15bb2b892
average-GitHub-enjoyer <nikinik20042@gmail.com>
2023-12-10 18:01:10 +0300

    Эта программа пользуется стандартным алгоритмом поиска кодона и их сравнения. Попробую написать новую программу с другими алгоритмами, а затем сравню их быстродействие.

--- dna.py
+++ dna.py
@@ -1,16 +1,47 @@
 # Нуклеотид состоит из букв A, C, G или T. Кодон состоит из трёх таких букв. Ген состоит из кодонов. Похоже, нужно сранивать кодоны в генах.
 
 class Codon:
-    def __init__(self, codon_sequence, contains_A, contains_C, contains_G, contains_T):
+    def __init__(self, codon_sequence):
         if len(codon_sequence) != 3:
             raise ValueError("Кодон должен иметь ровно 3 нуклеотида!")
         self.codon_sequence = codon_sequence
         # экспериментальная идея ниже: разделять кодон на содержащий тот или иной нуклеотид, чтобы уменьшить время поиска
-        self.contains_A = contains_A
-        self.contains_C = contains_C
-        self.contains_G = contains_G
-        self.contains_T = contains_T
-        # что-то типо флагов
+        # UPD: она мне не нравится
+
+    def __eq__(self, other):
+        # переопределение оператора равенства для сравнения кодонов
+        is_equal = isinstance(other, Codon) and self.form_aminos() == other.form_aminos()
+        return is_equal
+
+    def __ne__(self, other):
+        # переопределение оператора неравенства
+        return not self.__eq__(other)
+
+    def form_aminos(self):
+        # Определение аминокислот, которые формируются кодоном
+        genetic_code = {
+            "F": ["TTT", "TTC"],
+            "L": ["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"],
+            "I": ["ATT", "ATC", "ATA"],
+            "M": ["ATG"],
+            "V": ["GTT", "GTC", "GTA", "GTG"],
+            "S": ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+            "P": ["CCT", "CCC", "CCA", "CCG"],
+            "T": ["ACT", "ACC", "ACA", "ACG"],
+            "A": ["GCT", "GCC", "GCA", "GCG"],
+            "Y": ["TAT", "TAC"],
+            "H": ["CAT", "CAC"],
+            "Q": ["CAA", "CAG"],
+            "N": ["AAT", "AAC"],
+            "K": ["AAA", "AAG"],
+            "D": ["GAT", "GAC"],
+            "E": ["GAA", "GAG"],
+            "C": ["TGT", "TGC"],
+            "W": ["TGG"],
+            "R": ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],
+            "G": ["GGT", "GGC", "GGA", "GGG"]
+        }
+
 
 class Gene:
     def __init__(self, gene_sequence):
@@ -25,11 +56,7 @@ class Gene:
             self.gene_sequence = self.gene_sequence[:-2]
         for i in range(0, len(self.gene_sequence), 3):
             codon_sequence = self.gene_sequence[i:i + 3]
-            contains_A = 'A' in codon_sequence
-            contains_C = 'C' in codon_sequence
-            contains_G = 'G' in codon_sequence
-            contains_T = 'T' in codon_sequence
-            codon_list.append(Codon(codon_sequence, contains_A, contains_C, contains_G, contains_T))
+            codon_list.append(Codon(codon_sequence))
         return codon_list
 
     def find_codon(self, given_codon_sequence):
@@ -39,6 +66,12 @@ class Gene:
         return -1
 
 
-gen = Gene("TAGGGAТТAACCGТТATATATATATAGCCATGGATCGAТТATATAGGGAТТAACCGТТATATATATATAGCCATGGATCGAТТATA")
-a = gen.find_codon("ATA")
-print(a)
+
+# gen = Gene("TAGGGAТТAACCGТТATATATATATAGCCATGGATCGAТТATATAGGGAТТAACCGТТATATATATATAGCCATGGATCGAТТATA")
+# a = gen.find_codon("ATA")
+# print(a)
+
+codon1 = Codon("CGT")
+codon2 = Codon("AGA")
+
+print(codon1 == codon2)
